c0819737357fef50aa68b64840095a201a6df748
Pawel <pandius@poczta.fm>
2019-10-20 17:58:05 +0100

    created homepage with website logo

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import logo from '../pictures/logo.png'
+
+const Home = () => {
+    return (
+
+        <div className="home">
+            <img className="homeLogo" src={logo} alt="glamping Home page logo." />
+
+        </div>
+
+    );
+};
+export default Home;
