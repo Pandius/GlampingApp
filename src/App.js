499eee3814fcead7bdfd2ae0439da407e4b22de8
Pawel <pandius@poczta.fm>
2019-10-20 17:46:21 +0100

    created main app file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { Router } from '@reach/router'
-
+import Navigation from './components/Navigation'
+import Home from './pages/Home'
+import Sites from './pages/Sites'
+import SitesList from './pages/SitesList'
+import SitePage from './pages/SitePage'
+import Footer from './components/Footer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Header />
-      <Router>
-        <Home path='/' />
-        <Articles path='/sites' />
-        <ErrorPage default />
-      </Router>    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    feature: ""
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation searchBy={this.searchBy} />
+        <Router>
+          <Home path="/" />
+          <Sites path="/sites" />
+          <SitesList path="/sitesSerch" feature={this.state.feature} />
+          <SitesList path="/siteslist/:feature" />
+          <SitesList path="/siteslist" />
+          <SitePage path="/sites/:id" />
+        </Router>
+        <Footer />
+      </div>
+    );
+  }
+
+
+  searchBy = (word) => {
+    this.setState({ feature: word })
+  }
 }
 
 export default App;
