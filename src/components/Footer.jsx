aec3c930043000e10d014a817941cc9007c1d366
Pawel <pandius@poczta.fm>
2019-10-20 18:12:07 +0100

    created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h5>
+                © Pawel Bednarski 2019
+            </h5>
+        </div>
+    );
+};
+
+export default Footer;
